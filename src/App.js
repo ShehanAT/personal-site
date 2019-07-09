4a4ccf62740545076b676ef413e9b6282174502e
Shehan Atuk <shehanatukorala@Shehans-MacBook-Pro.local>
2019-07-09 12:22:12 -0400

    added hotmodulereplacemnent() for webpack

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.scss";
 import Header from "./components/header";
 import About from "./components/about";
 import Resume from "./components/resume";
+import { hot } from "react-hot-loader";
+
 // import "../public/favicon.ico";
 
 function App() {
