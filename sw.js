9269deba6d0eb52f9fbaa535a24c8b09cae3f524
Shehan Atuk <shehanatuk@gmail.com>
2020-04-17 21:01:22 -0400

    added linkedin to social icons

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4f64bc7d3d60b56eb2e3.js"
+    "url": "webpack-runtime-98ff3c2e29d12b46c614.js"
   },
   {
     "url": "framework-f455c5eaa02f0a0c2aab.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2fb1c3b0f5400023a3c7a6713bba83fe"
+    "revision": "e5011bfe3b89d2ed6d6578e7ec738027"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-4efef167a65f33edc4e5.js"
